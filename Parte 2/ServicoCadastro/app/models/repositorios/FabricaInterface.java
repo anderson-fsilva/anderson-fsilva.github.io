2e70c82bbec90c972f0a5a741048e6626eb84e9d
Diogo Dantas <dapd@cin.ufpe.br>
2018-11-06 08:19:06 -0300

    Containerização do Serviço de Cadastro da Funerária
    Faça o build da imagem: sudo docker image build -t confortame .
    Execute o container: sudo docker container run -p 9000:9000 -ti confortame bash
    Navegue para o diretório do serviço: cd ServicoCadastro
    Execute o sbt: ./sbt run

--- Parte 2/ServicoCadastro/app/models/repositorios/FabricaInterface.java
+++ Parte 2/ServicoCadastro/app/models/repositorios/FabricaInterface.java
@@ -1,16 +1,10 @@
 package models.repositorios;
 
 import java.util.List;
-import models.repositorios.pacote.IRepositorioPacote;
-import models.repositorios.pedido.IRepositorioPedido;
 import models.repositorios.funeraria.IRepositorioFuneraria;
 
 public interface FabricaInterface {
-		
-		public IRepositorioPacote makeRepPacote();
 
     	public IRepositorioFuneraria makeRepFuneraria();
-
-    	public IRepositorioPedido makeRepPedido();
 		
 }
--- Parte 2/ServicoCadastro/app/models/repositorios/FabricaRepositorioBDR.java
+++ Parte 2/ServicoCadastro/app/models/repositorios/FabricaRepositorioBDR.java
@@ -1,24 +1,12 @@
 package models.repositorios;
 
-import models.repositorios.pacote.IRepositorioPacote;
-import models.repositorios.pedido.IRepositorioPedido;
 import models.repositorios.funeraria.IRepositorioFuneraria;
-import models.repositorios.pacote.RepositorioPacoteBDR;
-import models.repositorios.pedido.RepositorioPedidoBDR;
 import models.repositorios.funeraria.RepositorioFunerariaBDR;
 
 public class FabricaRepositorioBDR implements FabricaInterface {
     
-    public IRepositorioPacote makeRepPacote(){
-        return new RepositorioPacoteBDR();
-    }
-    
     public IRepositorioFuneraria makeRepFuneraria(){
         return new RepositorioFunerariaBDR();
-    }
-
-    public IRepositorioPedido makeRepPedido(){
-    	return new RepositorioPedidoBDR();
     }
     
 }
