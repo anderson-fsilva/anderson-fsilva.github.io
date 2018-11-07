da297699b02f8a662e99ac6cb6d34a9df076e739
Diogo Dantas <dapd@cin.ufpe.br>
2018-11-07 20:30:20 -0300

    Comunicação entre serviços funcionando
    Faça o build da imagem: sudo docker image build -t confortame .
    Execute o container pro ServicoCadastro: sudo docker container run --rm --name servicocadastro -p 9000:9000 -ti confortame bash
    Execute o container pro ServicoGuiaRapido: sudo docker container run --rm --link servicocadastro:guiarapido -p 8080:9000 -ti confortame bash
    Em cada container, entre no diretorio do Servico do container e execute: ./sbt run

--- Parte 2/ServicoCadastro/app/models/repositorios/FabricaInterface.java
+++ Parte 2/ServicoCadastro/app/models/repositorios/FabricaInterface.java
@@ -2,7 +2,6 @@ package models.repositorios;
 
 import java.util.List;
 import models.repositorios.pacote.IRepositorioPacote;
-import models.repositorios.pedido.IRepositorioPedido;
 import models.repositorios.funeraria.IRepositorioFuneraria;
 
 public interface FabricaInterface {
@@ -10,7 +9,5 @@ public interface FabricaInterface {
 		public IRepositorioPacote makeRepPacote();
 
     	public IRepositorioFuneraria makeRepFuneraria();
-
-    	public IRepositorioPedido makeRepPedido();
 		
 }
--- Parte 2/ServicoCadastro/app/models/repositorios/FabricaRepositorioBDR.java
+++ Parte 2/ServicoCadastro/app/models/repositorios/FabricaRepositorioBDR.java
@@ -1,10 +1,8 @@
 package models.repositorios;
 
 import models.repositorios.pacote.IRepositorioPacote;
-import models.repositorios.pedido.IRepositorioPedido;
 import models.repositorios.funeraria.IRepositorioFuneraria;
 import models.repositorios.pacote.RepositorioPacoteBDR;
-import models.repositorios.pedido.RepositorioPedidoBDR;
 import models.repositorios.funeraria.RepositorioFunerariaBDR;
 
 public class FabricaRepositorioBDR implements FabricaInterface {
@@ -15,10 +13,6 @@ public class FabricaRepositorioBDR implements FabricaInterface {
     
     public IRepositorioFuneraria makeRepFuneraria(){
         return new RepositorioFunerariaBDR();
-    }
-
-    public IRepositorioPedido makeRepPedido(){
-    	return new RepositorioPedidoBDR();
     }
     
 }
